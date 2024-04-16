91da29d52064e0c32b3e4c85b15f256ee9dba239
AnasChaos <160777843+AnasChaos@users.noreply.github.com>
2024-04-16 22:45:40 +0500

    Scene 2 and Scene 3 Done With All Interections
    Scene 4 in progress

--- Assets/Scene2Handler.cs
+++ Assets/Scene2Handler.cs
@@ -1,15 +1,33 @@
 using Newtonsoft.Json;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Scene2Handler : MonoBehaviour
 {
     [SerializeField] Steps steps;
-    [SerializeField] int nextStepCounter;
-    [SerializeField] Image effectHover;
+    [SerializeField] int nextStepCounter = 26;
     [SerializeField] Camera cameraPos;
+    [SerializeField] GameObject inventory;
+
+    [Header("Step 26")]
+    [SerializeField] Image effectHover;
+
+    [Header("Step 27")]
+    [SerializeField] Animator animatorPapers;
+    [SerializeField] GameObject openBook;
+    [SerializeField] Image key2;
+
+    [Header("Step 28")]
+    [SerializeField] Image effectHover2;
+
+    [Header("Step 29")]
+    [SerializeField] GameObject chest;
+    [SerializeField] Animator animatorChest;
+    [SerializeField] Image daimonds;
+
+
 
 
     private void Start()
@@ -19,23 +37,34 @@ public class Scene2Handler : MonoBehaviour
         Debug.Log(steps);
         this.steps = JsonConvert.DeserializeObject<Steps>(steps);
         CameraPosAndSize(80);
-        StepCounter(26);
+        openBook.transform.localScale = Vector3.zero;
+        animatorPapers.transform.localScale = Vector3.zero;
+        ImageAlpha(effectHover, 0);
+        ImageAlpha(effectHover2, 0);
+        ImageAlpha(daimonds, 0);
+        animatorChest.transform.localScale = Vector3.zero;
     }
 
     private void StepCounter(int stepCounter)
     {
+        nextStepCounter++;
         switch (stepCounter)
         {
             case 26:
                 Step26();
                 break;
             case 27:
+                StartCoroutine(Step27());
                 break;
             case 28:
+                Step28();
                 break;
             case 29:
+                StartCoroutine(Step29());
                 break;
-
+            case 30:
+                Step30();
+                break;
             default:
                 break;
 
@@ -44,20 +73,100 @@ public class Scene2Handler : MonoBehaviour
 
     private void Step26()
     {
-        LeanTween.alpha(effectHover.rectTransform, 0.5f, 1.5f).setEase(LeanTweenType.easeInQuad).setOnComplete(() =>
+        LeanTween.alpha(effectHover.rectTransform, 0, 1.5f).setEase(LeanTweenType.easeInQuad).setOnComplete(() =>
         {
-            if (this.nextStepCounter == 28)
+            LeanTween.alpha(effectHover.rectTransform, 0.5f, 1.5f).setEase(LeanTweenType.easeInQuad).setOnComplete(() =>
             {
-                
-            }
-            else
-            {
-                Step26();
-            }
-            Debug.Log("Debug");
+                if (this.nextStepCounter == 28)
+                {
+                    effectHover.gameObject.SetActive(false);
+                }
+                else
+                {
+                    Step26();                   
+                }
+                Debug.Log("Debug");
+            });
         });
 
     }
+
+    private IEnumerator Step27()
+    {
+        animatorPapers.enabled = false;
+        animatorPapers.gameObject.SetActive(true);
+        LeanTween.scale(animatorPapers.gameObject, Vector2.one,2f).setOnComplete(() =>
+        {
+            animatorPapers.enabled = true;
+        });
+        yield return new WaitUntil(() => animatorPapers.GetCurrentAnimatorStateInfo(0).normalizedTime > 1);
+        LeanTween.moveY(inventory.gameObject, 50, 2f).setOnComplete(() =>
+        {   
+            openBook.SetActive(true);
+            LeanTween.scale(openBook, Vector3.one, 2f).setOnComplete(() =>
+            {
+                LeanTween.moveY(inventory.gameObject, -1000, 2f).setOnComplete(() =>
+                {
+                    LeanTween.alpha(key2.rectTransform, 1f, 2f).setEase(LeanTweenType.easeInQuad).setOnComplete(() =>
+                    {
+                        LeanTween.scale(animatorPapers.gameObject, Vector3.zero, 2f).setOnComplete(() =>
+                        {
+                            LeanTween.moveY(inventory.gameObject, 50, 2f).setOnComplete(() =>
+                            {
+                                LeanTween.scale(openBook.gameObject, Vector3.zero, 2f).setOnComplete(() =>
+                                {
+                                    LeanTween.moveY(inventory.gameObject, -1000, 2f).setOnComplete(() =>
+                                    {
+                                        NextStep();
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });               
+
+            });
+        });
+    }
+
+    private void Step28()
+    {
+        LeanTween.alpha(effectHover2.rectTransform, 0, 1.5f).setEase(LeanTweenType.easeInQuad).setOnComplete(() =>
+        {
+            LeanTween.alpha(effectHover2.rectTransform, 0.5f, 1.5f).setEase(LeanTweenType.easeInQuad).setOnComplete(() =>
+            {
+                if (this.nextStepCounter == 30)
+                {
+                    effectHover2.gameObject.SetActive(false);
+                }
+                else
+                {
+                      Step28();  
+                }
+            });
+        });
+    }
+   
+    private IEnumerator Step29()
+    {
+        animatorChest.enabled = false;
+        chest.gameObject.SetActive(true);
+        LeanTween.scale(chest, Vector3.one, 2f).setOnComplete(() =>
+        {
+            animatorChest.enabled = true;
+        });
+        yield return new WaitUntil(() => animatorChest.GetCurrentAnimatorStateInfo(0).normalizedTime > 1);
+        LeanTween.alpha(daimonds.rectTransform, 1f, 1f).setEase(LeanTweenType.easeInQuad).setDelay(2f).setOnComplete(() =>
+        {               
+            NextStep();
+        });
+    }
+
+    private void Step30() 
+    {
+        SceneManager.LoadScene("Cellar Game");
+    }
+
     private void CameraPosAndSize(int cameraSizeVal)
     {
         LeanTween.value(cameraPos.orthographicSize, cameraSizeVal, 2f)
@@ -67,7 +176,20 @@ public class Scene2Handler : MonoBehaviour
                 effectHover.gameObject.SetActive(false);
             }).setOnComplete(() =>
             {
-
+                StepCounter(26);
+                effectHover.gameObject.SetActive(true);
             });
+    }
+
+    public void NextStep() 
+    {
+        StepCounter(nextStepCounter);
+    }
+
+    private void ImageAlpha(Image img,float value) 
+    {
+        var color = img.color;
+        color.a = value;
+        img.color = color;
     }
 }
---
+++ Assets/Scene3Handler.cs
@@ -0,0 +1,241 @@
+using Newtonsoft.Json;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scene3Handler : MonoBehaviour
+{
+    [SerializeField] Steps steps;
+    [SerializeField] int nextStepCounter = 31;
+    [SerializeField] Camera cameraPos;
+    [SerializeField] GameObject inventory;
+
+    [Header("Step 31")]
+    [SerializeField] Image effectHover;
+
+    [Header("Step 32")]
+    [SerializeField] Image emptyJar;
+    [SerializeField] Image filledJar;
+    [SerializeField] Image emptyInventoryJar;
+    [SerializeField] Image filledInventoryJar;
+
+    [Header("Step 34")]
+    [SerializeField] Image effectHover2;
+
+    [Header("Step 36")]
+    [SerializeField] Image knifeInventory;
+    [SerializeField] Image knife;
+    [SerializeField] Image food;
+    [SerializeField] Image foodInventory;
+
+
+
+    private void Start()
+    {
+        cameraPos.orthographicSize = 40f;
+        string steps = Resources.Load<TextAsset>("Dialouges").ToString();
+        Debug.Log(steps);
+        this.steps = JsonConvert.DeserializeObject<Steps>(steps);
+        CameraPosAndSize(80);
+        StepCounter(31);
+        ImageAlpha(effectHover, 0);
+        emptyJar.transform.localScale = Vector3.zero;
+        ImageAlpha(filledJar, 0);
+        ImageAlpha(filledInventoryJar, 0);
+        ImageAlpha(effectHover2,0);
+        knifeInventory.gameObject.SetActive(false);
+        food.transform.localScale = Vector3.zero;
+        knife.transform.localScale = Vector3.zero;
+        ImageAlpha(foodInventory, 0); 
+
+    }
+
+    private void StepCounter(int stepCounter)
+    {
+        nextStepCounter++;
+        switch (stepCounter)
+        {
+            case 31:
+                Step31();
+                break;
+            case 32:
+                Step32();
+                break;
+            case 33:
+                Step33();
+                break;
+            case 34:
+                Step34();
+                break;
+            case 35:
+                Step35();
+                break;
+            case 36:
+                Step36();
+                break;
+            case 37:
+                Step37();
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    private void Step31()
+    {
+        LeanTween.alpha(effectHover.rectTransform, 0, 1.5f).setEase(LeanTweenType.easeInQuad).setOnComplete(() =>
+        {
+            LeanTween.alpha(effectHover.rectTransform, 0.5f, 1.5f).setEase(LeanTweenType.easeInQuad).setOnComplete(() =>
+            {
+                if (this.nextStepCounter == 33)
+                {
+                    effectHover.gameObject.SetActive(false);
+                }
+                else
+                {
+                    Step31();
+                }
+                Debug.Log("Debug");
+            });
+        });
+
+    }
+
+    private void Step32()
+    {
+        CameraPosAndSize(40, true, 900, 55f);
+    }
+
+    private void Step33()
+    {
+        LeanTween.moveY(inventory.gameObject, 10, 2f).setDelay(1).setOnComplete(() =>
+        {
+            LeanTween.scale(emptyJar.gameObject, new Vector3(2,2,2), 1.5f).setDelay(2).setOnComplete(() =>
+            {
+                LeanTween.alpha(filledJar.rectTransform, 1f, 2f).setDelay(1).setOnComplete(() =>
+                {
+                    emptyJar.gameObject.SetActive(false);
+                    LeanTween.scale(filledJar.gameObject, Vector2.zero, 2f).setDelay(1).setOnComplete(() =>
+                    {
+                        LeanTween.alpha(filledInventoryJar.rectTransform, 1f, 2f).setDelay(1).setOnComplete(() =>
+                        {
+                            LeanTween.moveY(inventory.gameObject, -50, 5f).setDelay(1).setOnComplete(() =>
+                            {
+                                CameraPosAndSize(80, true, 855, 91);
+                            });
+
+                        });
+
+                    });
+
+                });
+
+            });
+
+        });
+
+
+    }
+
+    private void Step34()
+    {
+        LeanTween.alpha(effectHover2.rectTransform, 0, 1.5f).setEase(LeanTweenType.easeInQuad).setOnComplete(() =>
+        {
+            LeanTween.alpha(effectHover2.rectTransform, 0.5f, 1.5f).setEase(LeanTweenType.easeOutQuad).setOnComplete(() =>
+            {
+                if (this.nextStepCounter == 36)
+                {
+                    effectHover2.gameObject.SetActive(false);
+                }
+                else
+                {
+                    Step34();
+                }
+                Debug.Log("Debug");
+            });
+        });
+
+    }
+
+    private void Step35()
+    {
+        CameraPosAndSize(40, true, 810, 61);
+    }
+    private void Step36()
+    {   
+        effectHover2.gameObject.SetActive(false);
+        knifeInventory.gameObject.SetActive(true);
+        LeanTween.moveY(inventory.gameObject, 20, 1.5f).setDelay(1).setOnComplete(() =>
+        {
+            LeanTween.scale(knife.gameObject, new Vector2(2, 2), 1.5f).setDelay(1).setOnComplete(() =>
+            {
+                LeanTween.rotateZ(knife.gameObject, 40, 1.5f).setDelay(1).setOnComplete(() =>
+                {
+                    LeanTween.scale(knife.gameObject, Vector2.zero, 1.5f).setDelay(1).setOnComplete(() =>
+                    {
+                        LeanTween.scale(food.gameObject, new Vector2(2, 2), 1.5f).setDelay(1).setOnComplete(() =>
+                        {
+                            LeanTween.scale(food.gameObject, Vector2.zero, 1.5f).setDelay(1).setOnComplete(() =>
+                            {
+                                LeanTween.alpha(foodInventory.rectTransform, 1, 1.5f).setDelay(1).setOnComplete(() =>
+                                {
+                                    LeanTween.moveY(inventory.gameObject, -50, 1.5f).setDelay(1).setOnComplete(() =>
+                                    {
+                                        CameraPosAndSize(80, true, 855, 91);
+                                    });
+
+                                });
+                            });
+                        });
+
+                    });
+
+                });
+            });
+        });
+
+    }
+
+    private void Step37() 
+    {
+        SceneManager.LoadScene("Cell Door");
+    }
+    public void NextStep()
+    {
+        StepCounter(nextStepCounter);
+    }
+
+
+    private void CameraPosAndSize(int cameraSizeVal,bool isPosChange = false,float valueX = 0, float valueY = 0)
+    {   //915 //55
+        if (isPosChange) 
+        {
+            LeanTween.move(cameraPos.gameObject, new Vector2(valueX, valueY), 1).setDelay(1).setOnComplete(() => 
+            {
+                NextStep();
+            });           
+        }
+       
+        LeanTween.value(cameraPos.orthographicSize, cameraSizeVal, 2f)
+        .setOnUpdate((float value) =>
+        {
+            cameraPos.orthographicSize = value;
+            effectHover.gameObject.SetActive(false);
+        }).setOnComplete(() =>
+        {            
+            effectHover.gameObject.SetActive(!isPosChange);
+        });
+        
+        
+    }
+
+    private void ImageAlpha(Image img, float value)
+    {
+        var color = img.color;
+        color.a = value;
+        img.color = color;
+    }
+
+}
